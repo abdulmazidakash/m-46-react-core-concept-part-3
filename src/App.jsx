abe4d24f3ae816c781f6f1a891d2d6b0a82321a0
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-11-03 21:56:46 +0600

    46-5

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 
 import './App.css'
-import HookForm from './components/HookForm/HookForm'
+import ReusableForm from './components/ReusableForm/ReusableForm'
+// import HookForm from './components/HookForm/HookForm'
 // import RefForm from './components/RefForm/RefForm'
 // import StateFulForm from './components/StateFulForm/StateFulForm'
 // import SimpleForm from './components/SimpleForm/SimpleForm'
 
 function App() {
 
+  
+	const handleSignUpSubmit = data =>{
+		console.log('sign up data', data);
+	}
+
+  const handleUpdateProfile = data =>{
+    console.log('update profile', data);
+  }
 
   return (
     <>
@@ -15,7 +24,29 @@ function App() {
       {/* <SimpleForm></SimpleForm> */}
       {/* <StateFulForm></StateFulForm> */}
       {/* <RefForm></RefForm> */}
-      <HookForm></HookForm>
+      {/* <HookForm></HookForm> */}
+      <ReusableForm 
+      formTitle={'sign up'} 
+      handleSubmit={handleSignUpSubmit}
+
+       >
+        <div>
+            <h2>Sign up</h2>
+            <p>please sign up right now</p>
+        </div>
+
+      </ReusableForm>
+      
+      <ReusableForm 
+      formTitle={'profile update'} 
+      handleSubmit={handleUpdateProfile} 
+      submitBtnText='Update' >
+        <div>
+            <h2>Update Profile</h2>
+            <p>Always keep your profile updated</p>
+        </div>
+
+      </ReusableForm>
       
     </>
   )
